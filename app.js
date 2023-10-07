54082991a7ec82164b7bf54e0f3cd21a5640fadd
Nikhil5922 <nikhilkumar7800@gmail.com>
2023-10-07 22:43:16 +0530

    add navbar

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app=express();
 const monoogse= require("mongoose");
 const Listing=require("./models/listing");
 const path=require("path");
-const { get } = require("http");
+const methodOverride=require("method-override");
+const ejsMate=require("ejs-mate");
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wonderlust";
 
@@ -21,6 +22,9 @@ async function main(){
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 app.get("/",(req,res)=>{
 res.send("Hi, I am root");
@@ -30,6 +34,7 @@ app.get("/listings", async(req,res)=>{
     const allListings=await Listing.find({});
     res.render("listings/index.ejs",{allListings});
 });
+
 app.get("/listings/new",(req,res)=>{
     res.render("listings/new.ejs");
 });
@@ -42,6 +47,22 @@ app.get("/listings/:id",async(req,res)=>{
 app.post("/listings",async(req,res)=>{
     let newListing=new Listing(req.body.listing);
     await newListing.save();
+    res.redirect("/listings");
+});
+app.get("/listings/:id/edit", async(req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+
+})
+app.put("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
+app.delete("/listings/:id", async(req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
 })
 app.listen(8080,()=>{
